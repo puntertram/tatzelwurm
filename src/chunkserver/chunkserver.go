ecf220f9cd3dce3b9a9a7b3df6e3c256cf49f8e7
Puneeth N Naik <puneethnaik60@gmail.com>
2024-12-14 19:22:35 +0530

    chunkserver define ChunkServerPersistentHashmap

--- src/chunkserver/chunkserver.go
+++ src/chunkserver/chunkserver.go
@@ -1,1 +1,22 @@
 package chunkserver
+
+import (
+	"fmt"
+	persistent_hashmap "tatzelwurm/utils"
+)
+
+type CHUNK_SERVER_CONFIG struct {
+	PERSISTENT_HASHMAP_WAL_DIRECTORY string `json:"PERSISTENT_HASHMAP_WAL_DIRECTORY"`
+	REPLICATION_FACTOR               int    `json:"REPLICATION_FACTOR"`
+	PORT                             int    `json:"PORT"`
+	GFS_CHUNK_SIZE                   int    `json:"GFS_CHUNK_SIZE"`
+}
+
+type ChunkServerPersistentHashmap struct {
+	persistent_hashmap.PersistentHashmap
+	config CHUNK_SERVER_CONFIG
+}
+
+func Run(chunkserver_config_file_path string) {
+	fmt.Println("Running as the chunkserver...")
+}
