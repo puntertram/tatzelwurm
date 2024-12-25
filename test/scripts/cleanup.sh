8c0a54a54aae933d2ab9f2f1e86089c71dc34b2f
Puneeth N Naik <puneethnaik60@gmail.com>
2024-12-25 21:25:42 +0530

    adapt the cleanup script for the new folder structure and add additional folders to clean

--- test/scripts/cleanup.sh
+++ test/scripts/cleanup.sh
@@ -1,18 +1,21 @@
 # This cleans up all the chunk files created and resets the state back to the original state
+cd /Users/puneeth/Documents/software/side-projects/gfs-clone
 rm data_store/chunkserver_one/*
 rm data_store/chunkserver_two/*
 rm data_store/chunkserver_three/*
 
-rm testing/output/*
+rm test/output/*
 
-# rm mainserver/chunk_server_map_audit.log
-rm mainserver/file_name_to_chunk_id_map_audit.log
-rm mainserver/persistent_hashmap_0_audit.log
-rm mainserver/persistent_hashmap_1_audit.log
-rm mainserver/persistent_hashmap_2_audit.log
+rm -rf client_meta/*
 
-# touch mainserver/chunk_server_map_audit.log
-touch mainserver/file_name_to_chunk_id_map_audit.log
-touch mainserver/persistent_hashmap_0_audit.log
-touch mainserver/persistent_hashmap_1_audit.log
-touch mainserver/persistent_hashmap_2_audit.log
+# rm mainserver_meta/chunk_server_map_audit.log
+rm mainserver_meta/file_name_to_chunk_id_map_audit.log
+rm mainserver_meta/persistent_hashmap_0_audit.log
+rm mainserver_meta/persistent_hashmap_1_audit.log
+rm mainserver_meta/persistent_hashmap_2_audit.log
+
+# touch mainserver_meta/chunk_server_map_audit.log
+touch mainserver_meta/file_name_to_chunk_id_map_audit.log
+touch mainserver_meta/persistent_hashmap_0_audit.log
+touch mainserver_meta/persistent_hashmap_1_audit.log
+touch mainserver_meta/persistent_hashmap_2_audit.log
