67d2ff36d287b2050a3c7a83c55e4794a9da2a65
Puneeth N Naik <puneethnaik60@gmail.com>
2025-01-04 13:20:28 +0530

    create the required folders for a cold start to the gfs system

--- test/scripts/setup.sh
+++ test/scripts/setup.sh
@@ -1,2 +1,20 @@
 mkdir -p data_store
 mkdir -p data_store/chunkserver_one
+mkdir -p data_store/chunkserver_two
+mkdir -p data_store/chunkserver_three
+
+mkdir -p client_meta
+
+mkdir -p chunkserver_meta/chunkserver_one
+mkdir -p chunkserver_meta/chunkserver_two
+mkdir -p chunkserver_meta/chunkserver_three
+touch chunkserver_meta/chunkserver_one/chunk_server_map_audit.log
+touch chunkserver_meta/chunkserver_two/chunk_server_map_audit.log
+touch chunkserver_meta/chunkserver_three/chunk_server_map_audit.log
+
+mkdir -p mainserver_meta
+touch mainserver_meta/chunk_server_map_audit.log
+touch mainserver_meta/file_name_to_chunk_id_map_audit.log
+touch mainserver_meta/persistent_hashmap_0_audit.log
+touch mainserver_meta/persistent_hashmap_1_audit.log
+touch mainserver_meta/persistent_hashmap_2_audit.log
