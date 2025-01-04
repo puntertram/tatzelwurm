5a8995d696f01e79cc1253ba2b330d5363231219
Puneeth N Naik <puneethnaik60@gmail.com>
2025-01-04 12:06:29 +0530

    cleanup chunkserver_meta folders

--- test/scripts/cleanup.sh
+++ test/scripts/cleanup.sh
@@ -13,9 +13,17 @@ rm mainserver_meta/file_name_to_chunk_id_map_audit.log
 rm mainserver_meta/persistent_hashmap_0_audit.log
 rm mainserver_meta/persistent_hashmap_1_audit.log
 rm mainserver_meta/persistent_hashmap_2_audit.log
+rm chunkserver_meta/chunkserver_one/chunk_server_map_audit.log
+rm chunkserver_meta/chunkserver_two/chunk_server_map_audit.log
+rm chunkserver_meta/chunkserver_three/chunk_server_map_audit.log
+
 
 # touch mainserver_meta/chunk_server_map_audit.log
 touch mainserver_meta/file_name_to_chunk_id_map_audit.log
 touch mainserver_meta/persistent_hashmap_0_audit.log
 touch mainserver_meta/persistent_hashmap_1_audit.log
 touch mainserver_meta/persistent_hashmap_2_audit.log
+touch chunkserver_meta/chunkserver_one/chunk_server_map_audit.log
+touch chunkserver_meta/chunkserver_two/chunk_server_map_audit.log
+touch chunkserver_meta/chunkserver_three/chunk_server_map_audit.log
+
