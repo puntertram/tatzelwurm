271d872aebc2cb012e2419a2e7feace09f869ab0
Puneeth N Naik <puneethnaik60@gmail.com>
2025-01-04 12:05:45 +0530

    refactor chunkserver request dto objects

--- src/model/api/request/chunkserver_request.go
+++ src/model/api/request/chunkserver_request.go
@@ -1,1 +1,8 @@
 package request_dto
+
+type SyncWithMainserverRequestChunkListModel = SyncFromChunkServerRequestChunkListModel
+
+type SyncWithMainserverRequestModel struct {
+	Chunk_list   []SyncWithMainserverRequestChunkListModel `json:"chunk_list"`
+	Ipv4_address string                                    `json:"ipv4_address"`
+}
